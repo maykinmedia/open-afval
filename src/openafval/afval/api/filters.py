4529fe14078596fbf97da046baf94662c4d1f544
Paul Schilling <mail@paulschilling.de>
2026-01-28 13:51:12 +0200

    feat: [#9] implement filters for afval resources
    Implemented filters for waste resources: date range, waste type, and
    address for ContainerLocation objects.

--- src/openafval/afval/api/filters.py
+++ src/openafval/afval/api/filters.py
@@ -1,17 +1,44 @@
 from django.utils.translation import gettext_lazy as _
 
-from django_filters.rest_framework import FilterSet, filters
+from django_filters import rest_framework
+
+from openafval.afval.models import Container, ContainerLocation, Lediging
 
 
-class BagBsnFilterSet(FilterSet):
-    bsn = filters.CharFilter(
-        help_text=_(
-            "Retrieves all the bag objects container emptying's "
-            "which are connected to an entities BSN."
-        ),
-        method="search_bsn",
+class ContainerFilterSet(rest_framework.FilterSet):
+    afval_type = rest_framework.CharFilter(
+        field_name="afval_type",
+        help_text=_("Filter containers by waste type (e.g., 'gft', 'restafval')"),
     )
 
-    # TODO: remove/improve when real data gets used.
-    def search_bsn(self, queryset, name, value):
-        return queryset
+    class Meta:
+        model = Container
+        fields = []
+
+
+class ContainerLocationFilterSet(rest_framework.FilterSet):
+    adres = rest_framework.CharFilter(
+        field_name="adres",
+        help_text=_("Filter container locations by address"),
+    )
+
+    class Meta:
+        model = ContainerLocation
+        fields = []
+
+
+class LedigingFilterSet(rest_framework.FilterSet):
+    startdatum = rest_framework.DateFilter(
+        field_name="geleegd_op_datum",
+        lookup_expr="gte",
+        help_text=_("Filter ledigingen from this date (YYYY-MM-DD)"),
+    )
+    einddatum = rest_framework.DateFilter(
+        field_name="geleegd_op_datum",
+        lookup_expr="lte",
+        help_text=_("Filter ledigingen until this date (YYYY-MM-DD)"),
+    )
+
+    class Meta:
+        model = Lediging
+        fields = []
